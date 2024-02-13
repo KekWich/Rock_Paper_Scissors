474fb7f64f01726e1dbe666eaecf41431f8e6534
Mewdex <155522630+KekWich@users.noreply.github.com>
2024-02-13 22:20:34 +0100

    Created basic file structure Created html boilerplate Attached scripts.js Wrote down pseudocode and planned project

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. 
+// Get computer to generate number
+// Asign the number to rock, papar, scissors
+// Show the result
+
+// 2. 
+// Get plyer input
+// Create if statemets that compare computer and player number
+// Make player selection case-insensitive
