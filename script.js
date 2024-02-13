e1b9b6532cc934e0c6d11d724aa63147ef66f45a
Mewdex <155522630+KekWich@users.noreply.github.com>
2024-02-13 22:59:49 +0100

    Created variable for random numbers Finished if statemets for 0, 1, 2 When received said number the text is shown

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. 
-// Get computer to generate number
+// Get computer to generate number - DONE
 // Asign the number to rock, papar, scissors
 // Show the result
 
@@ -7,3 +7,31 @@
 // Get plyer input
 // Create if statemets that compare computer and player number
 // Make player selection case-insensitive
+
+
+//3.
+// Create game that plays 5 rouns of RPS
+// Use loop till one player gets 3 wins
+
+
+
+//---------------------------------------------------------------------------//
+
+
+
+let randomNumber = Math.floor(Math.random() * 3);
+let rock
+
+
+if(randomNumber === 0) {
+  console.log('You have got rock');
+}
+
+else if(randomNumber === 1) {
+  console.log('You have got paper');
+}
+else {
+  console.log('You have got scissors');
+}
+
+console.log(randomNumber);
